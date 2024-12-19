27f84af1fc4b3c1ec936a34211b1223ee327262b
CrisCanapi730 <cristiancanapi730@gmail.com>
2024-12-19 14:29:49 -0400

    Con inicio de sesion

--- src/App.jsx
+++ src/App.jsx
@@ -1,97 +1,82 @@
-import { getAuth, signInAnonymously } from "firebase/auth";
-import { getToken, onMessage } from 'firebase/messaging';
-import React, { useEffect } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { ToastContainer, toast } from "react-toastify";
+import React, { useState } from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
-import { messaging } from './firebase';
 import Footer from './Footer';
 import Header from './Header';
 import Home from './Home';
 import Mascotas from './Mascotas';
-import Productos from './Productos';
 import Citas from './Citas';
+import Ventas from './Ventas';
+import Usuarios from './Usuarios'; // Importamos la vista Usuarios
+import Productos from './Productos'; // Importamos la vista Productos
+import IniciarSesion from './IniciarSesion';
+
 import './styles/App.css';
 import './styles/Footer.css';
 import './styles/Header.css';
-import Usuarios from './Usuarios';
-import Ventas from './Ventas';
-
-
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [usuarioLogueado, setUsuarioLogueado] = useState(null);
 
-   // Función de inicio de sesión anónimo
-   const loginAnonymously = () => {
-    signInAnonymously(getAuth())
-      .then((user) => console.log("Inicio de sesión anónimo exitoso", user))
-      .catch((error) => console.error("Error en inicio de sesión anónimo", error));
+  // Función para manejar el inicio de sesión
+  const login = (usuario) => {
+    setIsAuthenticated(true);
+    setUsuarioLogueado(usuario);
+    localStorage.setItem('usuario', JSON.stringify(usuario));
   };
 
-    // Función para activar el token de notificación
-    const enableNotifications = async () => {
-      try {
-        const token = await getToken(messaging, {
-          vapidKey: "BAn-7aFkPFpuVq1tjG1x3ox1VsnFgqQtLoV1uMY5-8V7axoy1U8VgUx7vIRTPUbtGbyCNlRiJn81QZNyPIHJQxA",
-        });
-        if (token) {
-          console.log("Token de notificación:", token);
-        } else {
-          console.warn("No se pudo obtener el token de notificación.");
-        }
-      } catch (error) {
-        console.error("Error al obtener el token de notificación:", error);
-      }
-    };
-  
-    // Registrar el Service Worker
-    useEffect(() => {
-      if ("serviceWorker" in navigator) {
-        navigator.serviceWorker
-          .register("/firebase-messaging-sw.js")
-          .then((registration) => {
-            console.log("Service Worker registrado exitosamente:", registration);
-          })
-          .catch((error) => {
-            console.error("Error al registrar el Service Worker:", error);
-          });
-      }
-  
-      onMessage(messaging, (message) => {
-        console.log("Mensaje en primer plano recibido:", message);
-  
-        // Mostrar notificación push en el navegador
-        if (Notification.permission === 'granted') {
-          new Notification(message.notification.title, {
-            body: message.notification.body,
-            icon: '/icon.png' // Asegúrate de tener un icono en tu proyecto
-          });
-        } else {
-          toast(message.notification.title); // Fallback en caso de que las notificaciones no estén habilitadas
-        }
-      });
-    }, []);
-    
+  // Función para manejar el cierre de sesión
+  const logout = () => {
+    setIsAuthenticated(false);
+    setUsuarioLogueado(null);
+    localStorage.removeItem('usuario');
+  };
+
   return (
     <div className="App">
-      <Header />
-      <main>
-        <ToastContainer />
-        {/* <button onClick={loginAnonymously}>Iniciar sesión anónimo</button>
-        <button onClick={enableNotifications}>Habilitar notificaciones</button> */}
-        <Routes>
-        <Route path="/" element={<Home />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/inicio" element={<Home />} />
-          <Route path="/inicio" element={<Home />} />
-          <Route path="/usuarios" element={<Usuarios />} />
-          <Route path="/mascotas" element={<Mascotas />} />
-          <Route path="/productos" element={<Productos />} />
-          <Route path="/citas" element={<Citas />} />
-          <Route path="/ventas" element={<Ventas />} />
-        </Routes>
-      </main>
-      <Footer />
+      {!isAuthenticated ? (
+        <IniciarSesion onLogin={login} />
+      ) : (
+        <>
+          <Header logout={logout} />
+          <main>
+            <ToastContainer />
+            <div className="auth-actions">
+              <button onClick={logout} className="btn">
+                Cerrar sesión
+              </button>
+            </div>
+            <Routes>
+              <Route path="/inicio" element={<Home />} />
+              <Route path="/ventas" element={<Ventas />} />
+              <Route path="/mascotas" element={<Mascotas usuarioLogueado={usuarioLogueado} />} />
+              <Route path="/citas" element={<Citas />} />
+
+              {/* Rutas para Administradores */}
+              {usuarioLogueado?.esAdmin && (
+                <>
+                  <Route path="/usuarios" element={<Usuarios />} />
+                  <Route path="/productos" element={<Productos />} />
+                </>
+              )}
+
+              {/* Rutas para usuarios regulares */}
+              {!usuarioLogueado?.esAdmin && (
+                <>
+                  <Route path="/usuarios" element={<Navigate to="/inicio" />} />
+                  <Route path="/productos" element={<Navigate to="/inicio" />} />
+                </>
+              )}
+
+              {/* Redirección a /inicio para rutas no existentes */}
+              <Route path="*" element={<Navigate to="/inicio" />} />
+            </Routes>
+          </main>
+          <Footer />
+        </>
+      )}
     </div>
   );
 }
---
+++ src/IniciarSesion.jsx
@@ -0,0 +1,231 @@
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
+import PropTypes from 'prop-types';
+import videoBackground from './assets/videovet.mp4'; // Ruta del video
+
+function IniciarSesion({ onLogin }) {
+  const [nombre, setNombre] = useState('');
+  const [correo, setCorreo] = useState('');
+  const [contrasena, setContrasena] = useState('');
+  const [modoRegistro, setModoRegistro] = useState(false);
+  const [listaUsuarios, setListaUsuarios] = useState([]);
+
+  useEffect(() => {
+    getUsuarios();
+  }, []);
+
+  const getUsuarios = () => {
+    Axios.get('https://api-vet-zeta.vercel.app/usuarios')
+      .then((response) => {
+        setListaUsuarios(response.data);
+      })
+      .catch((error) => {
+        console.error('Error al obtener usuarios:', error);
+      });
+  };
+
+  const iniciarSesion = () => {
+    if (!correo.trim() || !contrasena.trim()) {
+      alert('Por favor, ingresa tu correo y contraseña.');
+      return;
+    }
+
+    const usuarioEncontrado = listaUsuarios.find(
+      (usuario) =>
+        usuario.correo === correo && usuario.contrasena === contrasena
+    );
+
+    if (usuarioEncontrado) {
+      if (usuarioEncontrado.es_administrador) {
+        alert(`Bienvenido administrador, ${usuarioEncontrado.nombre}`);
+        localStorage.setItem('usuario', JSON.stringify(usuarioEncontrado));
+        onLogin({ ...usuarioEncontrado, esAdmin: 'admin' });
+      } else {
+        alert(`Bienvenido, ${usuarioEncontrado.nombre}`);
+        localStorage.setItem('usuario', JSON.stringify(usuarioEncontrado));
+        onLogin({ ...usuarioEncontrado, esAdmin: 'user' });
+      }
+    } else {
+      alert('Usuario o contraseña incorrectos.');
+    }
+  };
+
+  const registrarse = () => {
+    if (!nombre.trim() || !correo.trim() || !contrasena.trim()) {
+      alert('Por favor, completa todos los campos.');
+      return;
+    }
+
+    const usuarioExistente = listaUsuarios.find(
+      (usuario) => usuario.correo === correo
+    );
+    if (usuarioExistente) {
+      alert('El correo ya está registrado.');
+      return;
+    }
+
+    Axios.post('https://api-vet-zeta.vercel.app/create', {
+      nombre,
+      correo,
+      contrasena,
+    })
+      .then(() => {
+        alert('Usuario registrado exitosamente.');
+        setModoRegistro(false);
+        limpiarCampos();
+        getUsuarios();
+      })
+      .catch((error) => {
+        console.error('Error al registrar usuario:', error);
+      });
+  };
+
+  const limpiarCampos = () => {
+    setNombre('');
+    setCorreo('');
+    setContrasena('');
+  };
+
+  const styles = {
+    videoBackground: {
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      objectFit: 'cover',
+      zIndex: -1,
+    },
+    container: {
+      maxWidth: '400px',
+      margin: '50px auto',
+      padding: '20px',
+      backgroundColor: 'rgba(255, 255, 255, 0.85)',
+      borderRadius: '8px',
+      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+    },
+    heading: {
+      fontSize: '24px',
+      textAlign: 'center',
+      marginBottom: '20px',
+      color: '#000', // Letras negras
+    },
+    title: {
+      fontSize: '32px',
+      textAlign: 'center',
+      color: '#DAA520', // Dorado
+      marginTop: '20px',
+      marginBottom: '10px',
+    },
+    label: {
+      display: 'block',
+      marginBottom: '15px',
+    },
+    input: {
+      width: 'calc(100% - 10px)',
+      padding: '10px',
+      fontSize: '16px',
+      marginTop: '5px',
+      border: '1px solid #ccc',
+      borderRadius: '4px',
+      boxSizing: 'border-box',
+    },
+    button: {
+      display: 'inline-block',
+      width: '100%',
+      padding: '10px',
+      fontSize: '16px',
+      color: '#fff',
+      backgroundColor: '#2196f3', // Celeste
+      border: 'none',
+      borderRadius: '4px',
+      cursor: 'pointer',
+      transition: 'background-color 0.3s',
+      marginTop: '10px',
+    },
+    buttonSecondary: {
+      backgroundColor: '#2196f3', // Celeste
+    },
+  };
+
+  return (
+    <>
+      <video style={styles.videoBackground} autoPlay loop muted>
+        <source src={videoBackground} type="video/mp4" />
+        Tu navegador no soporta el video.
+      </video>
+      <div style={styles.title}>
+        <h1>Veterinaria Patito</h1>
+      </div>
+      <div style={styles.container}>
+        <h1 style={styles.heading}>
+          {modoRegistro ? 'Registrarse' : 'Iniciar Sesión'}
+        </h1>
+
+        {modoRegistro && (
+          <label style={styles.label}>
+            Nombre:
+            <input
+              type="text"
+              value={nombre}
+              onChange={(e) => setNombre(e.target.value)}
+              placeholder="Ingresa tu nombre"
+              style={styles.input}
+            />
+          </label>
+        )}
+
+        <label style={styles.label}>
+          Correo:
+          <input
+            type="email"
+            value={correo}
+            onChange={(e) => setCorreo(e.target.value)}
+            placeholder="Ingresa tu correo"
+              style={styles.input}
+          />
+        </label>
+
+        <label style={styles.label}>
+          Contraseña:
+          <input
+            type="password"
+            value={contrasena}
+            onChange={(e) => setContrasena(e.target.value)}
+            placeholder="Ingresa tu contraseña"
+            style={styles.input}
+          />
+        </label>
+
+        <div>
+          {modoRegistro ? (
+            <button style={styles.button} onClick={registrarse}>
+              Registrarse
+            </button>
+          ) : (
+            <button style={styles.button} onClick={iniciarSesion}>
+              Iniciar Sesión
+            </button>
+          )}
+          <button
+            style={{
+              ...styles.button,
+              ...styles.buttonSecondary,
+            }}
+            onClick={() => setModoRegistro(!modoRegistro)}
+          >
+            {modoRegistro
+              ? '¿Ya tienes una cuenta? Inicia Sesión'
+              : '¿No tienes cuenta? Regístrate'}
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+IniciarSesion.propTypes = {
+  onLogin: PropTypes.func.isRequired,
+};
+
+export default IniciarSesion;
